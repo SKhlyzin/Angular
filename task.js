844e06ef4294d34a271ae60c924b77f34e223d6d
SKhlyzin <31540686+SKhlyzin@users.noreply.github.com>
2018-04-23 15:53:24 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,3 +1,7 @@
+/*4*/
+let obb = [1,2,3];
+ob.reduce((prev, cur, i) => i%2  && prev + cur || prev, 0);
+2
 /*5*/
 function calc(str) {
     return /^(?:\d+([*+-]|\/(?!0)))+\d+$/.test(str) ? eval(str): false;
