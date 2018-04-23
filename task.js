0947f08bcfe4a7f8f845575b9c8777c444685530
SKhlyzin <31540686+SKhlyzin@users.noreply.github.com>
2018-04-23 17:30:57 +0300

    Update task.js

--- task.js
+++ task.js
@@ -53,3 +53,11 @@ var counter = makeCounter();
 console.log(counter());
 console.log(counter());
 console.log(counter());
+/*7*/
+var take = function(fn, count){
+ var arr = [];
+ for(var i=0; i<count; i++){
+  arr[i] = fn();
+ }
+ return arr;
+}
