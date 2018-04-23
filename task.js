11a6520c7bd0aadb19d28ca0d8d4636a115c246b
SKhlyzin <31540686+SKhlyzin@users.noreply.github.com>
2018-04-23 17:20:45 +0300

    Update task.js

--- task.js
+++ task.js
@@ -19,6 +19,19 @@
 	} 
 	return compressed;
 };
+/*2*/
+var f = arr => [arr[0], arr[2], arr[arr.length-1]];
+var [a,b,n] = f([1, 2, 3, 4,5,6,14,15]);
+console.log(a);
+console.log(b);
+console.log(n);
+/*3*/
+var f = arr => {if (arr.length == 3) return [arr[0], arr[1], arr[2]]
+else return [arr[0], arr[1], false]};
+var [a,b,c] = f([5,6]);
+console.log(a);
+console.log(b);
+console.log(c);
 /*4*/
 let ob = [1,2,3];
 ob.reduce((prev, cur, i) => i%2  && prev + cur || prev, 0);
