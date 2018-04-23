b2150c40c95dea8bf12083469bcd777751d50d55
SKhlyzin <31540686+SKhlyzin@users.noreply.github.com>
2018-04-23 17:19:14 +0300

    Update task.js

--- task.js
+++ task.js
@@ -1,7 +1,24 @@
 /*1*/
-let customers = a.filter(function(person) {
-  return person.user_id == '22341';
-});
+  function compressArray(original) { 
+	var compressed = [];
+	var copy = original.slice(0);
+	for (var i = 0; i < original.length; i++) { 
+		var myCount = 0;	
+		for (var w = 0; w < copy.length; w++) {
+			if (original[i] == copy[w]) {
+				myCount++;
+				delete copy[w];
+			}
+		}
+		if (myCount > 0) {
+			var a = new Object();
+			a.value = original[i];
+			a.count = myCount;
+			compressed.push(a);
+		}
+	} 
+	return compressed;
+};
 /*4*/
 let ob = [1,2,3];
 ob.reduce((prev, cur, i) => i%2  && prev + cur || prev, 0);
